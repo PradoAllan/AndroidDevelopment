8e2d3c192a7dcbf6a18ae4b59d00aa68ac1e596d
PradoAllan <allanprado.it@gmail.com>
2024-05-18 15:21:04 -0300

    feat: great user input

--- gettingStarted/app/src/main/java/com/hfad/myfirstapp/MainActivity.kt
+++ gettingStarted/app/src/main/java/com/hfad/myfirstapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hfad.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView)
+        val myName = findViewById<EditText>(R.id.editTextText)
+        val sendButton = findViewById<Button>(R.id.button)
+        sendButton.setOnClickListener {
+            text.text = "Hello, ${myName.text}!"
+            /*
+            var oldText = text.text
+            var input = myName.text
+            var newText = "$oldText $input"
+            text.text = newText
+             */
+        }
     }
 }
