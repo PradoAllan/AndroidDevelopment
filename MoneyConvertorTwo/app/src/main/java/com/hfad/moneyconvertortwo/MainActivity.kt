7d5c86b33373331fc5a04761cdc0425ad861dc4f
PradoAllan <allanprado.it@gmail.com>
2024-08-10 13:05:48 -0300

    up: UI colors

--- MoneyConvertorTwo/app/src/main/java/com/hfad/moneyconvertortwo/MainActivity.kt
+++ MoneyConvertorTwo/app/src/main/java/com/hfad/moneyconvertortwo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hfad.moneyconvertortwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    var moneyArray = arrayOf("USD", "CAD", "EUR", "BRL")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val refSpinner = findViewById<Spinner>(R.id.currency)
+
+
     }
 }
