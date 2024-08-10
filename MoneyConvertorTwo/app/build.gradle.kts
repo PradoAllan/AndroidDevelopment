b2f6815bdb9f604f747ee550bcac37a570ddb5c6
PradoAllan <allanprado.it@gmail.com>
2024-08-10 14:30:26 -0300

    feat: to conversion DONE

--- MoneyConvertorTwo/app/build.gradle.kts
+++ MoneyConvertorTwo/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hfad.moneyconvertortwo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hfad.moneyconvertortwo"
--- MoneyConvertorTwo/app/src/main/java/com/hfad/moneyconvertortwo/MainActivity.kt
+++ MoneyConvertorTwo/app/src/main/java/com/hfad/moneyconvertortwo/MainActivity.kt
@@ -3,16 +3,38 @@ package com.hfad.moneyconvertortwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var moneyArray = arrayOf("USD", "CAD", "EUR", "BRL")
+
+    private fun Double.format(digits: Int) = "%.${digits}f".format(this)
+    private fun getMoneyValue(s: String) : Double {
+        return when (s) {
+            "USD" -> (0.18)
+            "CAD" -> (0.25)
+            "EUR" -> (0.17)
+            "GBP" -> (0.14)
+            else -> (0.0)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val refSpinner = findViewById<Spinner>(R.id.currency)
+        val refButton = findViewById<Button>(R.id.convert)
+        val refAmout = findViewById<EditText>(R.id.inputValue)
+        val refResult = findViewById<TextView>(R.id.result)
+        val refCalc = findViewById<TextView>(R.id.example)
+        refButton.setOnClickListener {
+            var result : Double
 
-
+            result = ((refAmout.text.toString().toDouble()) * getMoneyValue(refSpinner.selectedItem.toString()))
+            refCalc.text = "Conversion: ${refAmout.text} * ${getMoneyValue(refSpinner.selectedItem.toString()).toString()}"
+            refResult.text = "Final value: ${result.format(2).toString()}"
+        }
     }
 }
