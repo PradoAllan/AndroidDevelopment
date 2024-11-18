aff454e4cfdb794516cde9527565491f471ed067
PradoAllan <allanprado.it@gmail.com>
2024-11-18 12:25:11 -0300

    fix: saving instance state DONE

--- UnderstandingActivityLifeCycle/app/src/main/java/com/hfad/understandingactivitylifecycle/MainActivity.kt
+++ UnderstandingActivityLifeCycle/app/src/main/java/com/hfad/understandingactivitylifecycle/MainActivity.kt
@@ -5,79 +5,149 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import java.text.ChoiceFormat
 
 class MainActivity : AppCompatActivity() {
 
-    private var baseTime: Long = 0
-    //SystemClock.elapsedRealtime()
-    private var isRunning: Boolean = false
-    private lateinit var chronometerRef: Chronometer
-    // = findViewById<Chronometer>(R.id.stopwatch)
+    lateinit var chronometer: Chronometer
+    var isRunning = false
+    var offSet: Long = 0
+
+    fun setBaseTime()
+    {
+        chronometer.base = SystemClock.elapsedRealtime() - offSet
+    }
+
+    fun saveOffSet()
+    {
+        offSet = SystemClock.elapsedRealtime() - chronometer.base
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startButtonRef = findViewById<Button>(R.id.startButton)
-        val pauseButtonRef = findViewById<Button>(R.id.pauseButton)
-        val resetButtonRef = findViewById<Button>(R.id.resetButton)
-        chronometerRef = findViewById<Chronometer>(R.id.stopwatch)
-
-        var savedState: Long = 0
-        var savedBaseTime: Long = 0
-        var runningState: Boolean = false
+        chronometer = findViewById<Chronometer>(R.id.stopwatch)
 
         if (savedInstanceState != null)
         {
-            savedState = savedInstanceState.getLong("current")
-            runningState = savedInstanceState.getBoolean("running", false)
-            savedBaseTime = savedInstanceState.getLong("baseTime")
-            if (runningState)
+            isRunning = savedInstanceState.getBoolean("running")
+            offSet = savedInstanceState.getLong("offset")
+            if (isRunning)
             {
-                chronometerRef.base = savedState
-                chronometerRef.start()
+                chronometer.base = savedInstanceState.getLong("currentState")
+                chronometer.start()
             }
             else
-                chronometerRef.base = baseTime
+                setBaseTime()
         }
 
-        isRunning = runningState
-        baseTime = savedState
+        //chronometer = findViewById<Chronometer>(R.id.stopwatch)
 
-        startButtonRef.setOnClickListener() {
+        val startButtonRef = findViewById<Button>(R.id.startButton)
+        startButtonRef.setOnClickListener {
             if (!isRunning)
             {
                 isRunning = true
-                if (baseTime != 0.toLong())
-                {
-                    chronometerRef.base = SystemClock.elapsedRealtime() - baseTime
-                }
-                else
-                    chronometerRef.base = SystemClock.elapsedRealtime()
-                chronometerRef.start()
+                setBaseTime()
+                chronometer.start()
             }
         }
 
-        pauseButtonRef.setOnClickListener() {
+        val pauseButtonRef = findViewById<Button>(R.id.pauseButton)
+        pauseButtonRef.setOnClickListener {
             if (isRunning)
             {
                 isRunning = false
-                baseTime = SystemClock.elapsedRealtime() - chronometerRef.base
-                chronometerRef.stop()
+                saveOffSet()
+                chronometer.stop()
             }
         }
 
-        resetButtonRef.setOnClickListener() {
-            baseTime = 0
-            chronometerRef.base = SystemClock.elapsedRealtime()
+        val resetButtonRef = findViewById<Button>(R.id.resetButton)
+        resetButtonRef.setOnClickListener {
+            offSet = 0
+            setBaseTime()
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        //outState.putLong("current", chronometerRef.base)
-        super.onSaveInstanceState(outState)
-        outState.putLong("baseTime", baseTime)
-        outState.putLong("current", chronometerRef.base)
+        outState.putLong("currentState", chronometer.base)
+        outState.putLong("offset", offSet)
         outState.putBoolean("running", isRunning)
+        super.onSaveInstanceState(outState)
     }
+
+//    private var baseTime: Long = 0
+//    //SystemClock.elapsedRealtime()
+//    private var isRunning: Boolean = false
+//    private lateinit var chronometerRef: Chronometer
+//    // = findViewById<Chronometer>(R.id.stopwatch)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val startButtonRef = findViewById<Button>(R.id.startButton)
+//        val pauseButtonRef = findViewById<Button>(R.id.pauseButton)
+//        val resetButtonRef = findViewById<Button>(R.id.resetButton)
+//        chronometerRef = findViewById<Chronometer>(R.id.stopwatch)
+//
+//        var savedState: Long = 0
+//        var savedBaseTime: Long = 0
+//        var runningState: Boolean = false
+//
+//        if (savedInstanceState != null)
+//        {
+//            savedState = savedInstanceState.getLong("current")
+//            runningState = savedInstanceState.getBoolean("running", false)
+//            savedBaseTime = savedInstanceState.getLong("baseTime")
+//            if (runningState)
+//            {
+//                chronometerRef.base = savedState
+//                chronometerRef.start()
+//            }
+//            else
+//                chronometerRef.base = SystemClock.elapsedRealtime() - savedState
+//        }
+//
+//        isRunning = runningState
+//        baseTime = savedState
+//
+//        startButtonRef.setOnClickListener() {
+//            if (!isRunning)
+//            {
+//                isRunning = true
+//                if (baseTime != 0.toLong())
+//                {
+//                    chronometerRef.base = SystemClock.elapsedRealtime() - baseTime
+//                }
+//                else
+//                    chronometerRef.base = SystemClock.elapsedRealtime()
+//                chronometerRef.start()
+//            }
+//        }
+//
+//        pauseButtonRef.setOnClickListener() {
+//            if (isRunning)
+//            {
+//                isRunning = false
+//                baseTime = SystemClock.elapsedRealtime() - chronometerRef.base
+//                chronometerRef.stop()
+//            }
+//        }
+//
+//        resetButtonRef.setOnClickListener() {
+//            baseTime = 0
+//            chronometerRef.base = SystemClock.elapsedRealtime()
+//        }
+//    }
+//
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        //outState.putLong("current", chronometerRef.base)
+//        super.onSaveInstanceState(outState)
+//        outState.putLong("baseTime", baseTime)
+//        outState.putLong("current", chronometerRef.base)
+//        outState.putBoolean("running", isRunning)
+//    }
 
 }
