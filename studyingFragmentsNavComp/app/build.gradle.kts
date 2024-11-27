6902726eaffba10adf7dbbbecc9aa7f36bf765c6
PradoAllan <allanprado.it@gmail.com>
2024-11-27 07:54:47 -0300

    add: navigation component dependency

--- studyingFragmentsNavComp/app/build.gradle.kts
+++ studyingFragmentsNavComp/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-common-ktx:2.3.5")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
--- studyingFragmentsNavComp/build.gradle.kts
+++ studyingFragmentsNavComp/build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+
+
 }
+
+//Por algum motivo, estava dando algum problema ao criar essa variavel com a versao do nav component.
+//buildscript {
+//    ext.nav_version = "2.3.5"
+//}
