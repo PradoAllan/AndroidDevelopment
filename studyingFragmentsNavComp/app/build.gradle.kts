d99a29299a2c17e41b8feed0be9958e05267e740
PradoAllan <allanprado.it@gmail.com>
2024-11-27 08:36:44 -0300

    fix: nav graph problem. now adding all fragments

--- studyingFragmentsNavComp/app/build.gradle.kts
+++ studyingFragmentsNavComp/app/build.gradle.kts
@@ -37,7 +37,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-common-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    //implementation("androidx.navigation:navigation-common-ktx:2.8.4")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
