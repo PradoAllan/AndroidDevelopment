4104a72328b07b78306b61c5f7e7f33014f0fe31
PradoAllan <allanprado.it@gmail.com>
2025-01-28 19:25:23 -0300

    feat: add new fragments and nav graph

--- CatChat/app/src/main/java/com/hfad/catchat/MainActivity.kt
+++ CatChat/app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hfad.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolBar = findViewById<MaterialToolbar>(R.id.materialToolbar)
+        setSupportActionBar(toolBar)
+
     }
 }
