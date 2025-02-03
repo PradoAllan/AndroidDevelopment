9671fcf6e660d75ec8eba93d9f5ce00f697863d3
PradoAllan <allanprado.it@gmail.com>
2025-02-03 19:28:14 -0300

    add: menu resource with help item

--- CatChat/app/src/main/java/com/hfad/catchat/MainActivity.kt
+++ CatChat/app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -2,6 +2,10 @@ package com.hfad.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -13,4 +17,16 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolBar)
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean
+//    {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+//    }
 }
